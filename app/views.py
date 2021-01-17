23228c9c1eac0b110e6c1848f157325e1ff8f416
Juan Londono <jlondono@gmail.com>
2021-01-17 14:05:26 +1000

    Add close price graph, and label and title to forecast graph

--- app/views.py
+++ app/views.py
@@ -3,20 +3,23 @@ from app import app
 from app.ticker_form import TickerForm
 from app.stock_predictor import forecaster
 
+
 @app.route('/')
 def home():
-  form = TickerForm()
-  return render_template('home.html', form=form)
+    form = TickerForm()
+    return render_template('home.html', form=form)
+
 
 @app.route('/ticker', methods=['POST'])
 def ticker():
-   form = TickerForm()
+    form = TickerForm()
 
-   if not form.validate_on_submit():
-      return redirect('/')
+    if not form.validate_on_submit():
+        return redirect('/')
 
-   forecast_info = forecaster(form.ticker.data, form.days.data)
-   print(forecast_info['forecast'])
-   forecast = forecast_info['forecast'].itertuples()
-   
-   return render_template('results.html', ticker=form.ticker.data, days=form.days.data, forecast=forecast, fig_components=forecast_info['fig_components'], fig_forecast=forecast_info['fig_forecast'])
+    forecast_info = forecaster(form.ticker.data, form.days.data)
+
+    print(forecast_info['forecast'])
+    forecast = forecast_info['forecast'].itertuples()
+
+    return render_template('results.html', ticker=form.ticker.data, days=form.days.data, forecast=forecast, fig_paths=forecast_info['fig_paths'])
