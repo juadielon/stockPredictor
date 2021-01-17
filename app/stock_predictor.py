48b9493626d589375606772cd0e1d4da5fe25869
Juan Londono <juan.londono@uq.edu.au>
2021-01-17 14:05:26 +1000

    Add close price graph, and label and title to forecast graph

--- app/stock_predictor.py
+++ app/stock_predictor.py
@@ -1,6 +1,9 @@
 import yfinance as yf
 import pandas as pd
+from matplotlib import pyplot as plt
 from fbprophet import Prophet
+from datetime import datetime
+
 
 def forecaster(ticker, periods):
     """
@@ -12,14 +15,14 @@ def forecaster(ticker, periods):
     """
 
     periods = int(periods)
- 
+
     # Get historical data from Yahoo Finance
     stock_data = yf.Ticker(ticker)
     # Yahoo Finance allows to retrieve historical data for:
-    # 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max 
+    # 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
     historical_data = stock_data.history('max', auto_adjust=True)
 
-    # Prophet requires the dates (ds) and adjusted closing prices (y)  
+    # Prophet requires the dates (ds) and adjusted closing prices (y)
     # Create new data frame with the required data
     df = pd.DataFrame()
     df['ds'] = historical_data.index.values
@@ -36,19 +39,42 @@ def forecaster(ticker, periods):
 
     future = model.make_future_dataframe(periods, freq='D')
     # As the stock exchange is closed on weekends, remove weekends in the future
-    new_future = future[future['ds'].dt.dayofweek<5]
+    new_future = future[future['ds'].dt.dayofweek < 5]
     # As some days were removed, recalculate number of available periods to display
     available_periods = periods - (len(future) - len(new_future))
     forecast = model.predict(new_future)
 
-    # Save graphs 
-    fig_components = '/static/img/components_' + ticker +  '.png'
-    model.plot_components(forecast).savefig('../app' + fig_components)
+    # Save graphs
+    fig_location = '/static/img/figures/'
+    fig_paths = {
+        'price': fig_location + 'price_' + ticker + '.png',
+        'components': fig_location + 'components_' + ticker + '.png',
+        'forecast': fig_location + 'forecast_' + ticker + '.png'
+    }
 
-    fig_forecast = '/static/img/forecast_' + ticker + '.png'
-    model.plot(forecast).savefig('../app' + fig_forecast)
+    # Price
+    fig_price = plt.figure(facecolor='w', figsize=(20, 6))
+    plt.title(ticker + ' close price', fontsize=20)
+    plt.ylabel('Price', fontsize=15)
+    plt.plot(df['ds'], df['y'])
+    fig_price.savefig('../app' + fig_paths['price'])
 
+    # Components
+    model.plot_components(forecast).savefig('../app' + fig_paths['components'])
+
+    # Forecast
+    # model.plot(forecast).savefig('../app' + fig_paths['forecast'])
+    fig_forecast = model.plot(forecast)
+    # plt.margins(x=0)
+    plt.title(ticker + ' forecast', fontsize=10, pad=1)
+    plt.xlabel('Day', fontsize=10)
+    plt.ylabel('Price', fontsize=10)
+    date_now = datetime.now()
+    plt.axvline(date_now, color="k", linestyle=":")
+    fig_forecast.savefig('../app' + fig_paths['forecast'])
+
+    # Return requested period
     # forecast = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(available_periods+1)
     forecast = forecast.tail(available_periods+1)
 
-    return {'forecast': forecast, 'fig_components': fig_components, 'fig_forecast': fig_forecast} 
+    return {'forecast': forecast, 'fig_paths': fig_paths}
